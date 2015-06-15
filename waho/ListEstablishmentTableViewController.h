bc72102de823c52da5e5a882da796ddf9927728b
José Luiz Correia Neto <joseluizcorreianeto@Ze-Luiz-Macbook.local>
2015-06-15 17:18:59 -0300

    Tableview para a lista de estabelecimentos e lista de estabelecimentos salvos funcionando corretamente

---
+++ waho/ListEstablishmentTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  tableViewEstablishmentTableViewController.h
+//  waho
+//
+//  Created by José Luiz Correia Neto on 15/06/15.
+//  Copyright (c) 2015 Miguel Araújo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListEstablishmentTableViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
