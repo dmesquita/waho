cff46f52d3826fc284eda3da717bfbb7ac379c23
Déborah Mesquita <deh.mesquita@hotmail.com>
2015-09-20 15:09:43 -0300

    Auto layout for Favorites View done

---
+++ waho/FavoriteTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  FavoriteTableViewCell.h
+//  waho
+//
+//  Created by Déborah Mesquita on 20/09/15.
+//  Copyright (c) 2015 Miguel Araújo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoriteTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintCategorySpace;
+
+
+@end
---
+++ waho/FavoriteTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  FavoriteTableViewCell.m
+//  waho
+//
+//  Created by Déborah Mesquita on 20/09/15.
+//  Copyright (c) 2015 Miguel Araújo. All rights reserved.
+//
+
+#import "FavoriteTableViewCell.h"
+
+@implementation FavoriteTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
